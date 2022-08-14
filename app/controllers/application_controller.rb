b157bdacb053c8dc72b2f73dcc6f011fe3b165b1
Abel Zazueta <abzazuet@amazon.com>
2022-08-13 18:45:47 -0700

    Backend workds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,89 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
+  set :default_content_type, "application/json"
+
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  get "/desserts" do
+    Dessert.desserts.to_json
+  end
+  get "/desserts/:id" do
+    dessert = Dessert.find(params[:id])
+    dessert.dessert_obj.to_json
+  end
+  post "/desserts" do
+    Dessert.create(
+      name: params[:name],
+      image: params[:image],
+      carbohydrates: params[:carbohydrates],
+      proteins: params[:proteins],
+      fat: params[:fat],
+    )
+    dessert_id = Dessert.last.id
+    params[:ingredients].each do |ingredient|
+      ingredient = Ingredient.find_or_create_by(name: ingredient)
+      Recipe.create(
+        dessert_id: dessert_id,
+        ingredient_id: ingredient.id,
+      )
+    end
+    params[:regimes].each do |regime|
+      regime = Regime.find_or_create_by(name: regime)
+      Dessert_Regime.create(
+        dessert_id: dessert_id,
+        regime_id: regime.id,
+      )
+    end
+    Dessert.last.dessert_obj.to_json
+  end
 
+  delete "/desserts/:id" do
+    recipe = Recipe.where("dessert_id = ?", params[:id])
+    dessert_regime = Dessert_Regime.where("dessert_id = ?", params[:id])
+    dessert = Dessert.find(params[:id])
+    dessert.destroy
+    dessert_regime.destroy_all
+    recipe.destroy_all
+    dessert.to_json
+  end
+
+  patch "/desserts/:id" do
+    dessert = Dessert.find(params[:id])
+    dessert.update(
+      name: params[:name],
+      image: params[:image],
+      proteins: params[:proteins],
+      carbohydrates: params[:carbohydrates],
+      fat: params[:fat],
+    )
+    new_ingredients = params[:ingredients]
+    dessert.ingredients.each do |ingredient|
+      if (!new_ingredients.include?(ingredient))
+        ingredient_entry = Ingredient.find_by(name: ingredient)
+        Recipe.where("dessert_id = ? AND ingredient_id = ?", dessert.id, ingredient_entry.id).delete_all
+      end
+    end
+    new_ingredients.each do |new_ingredient|
+      ingredient = Ingredient.find_or_create_by(name: new_ingredient)
+      if Recipe.where("dessert_id = ? AND ingredient_id = ?", dessert.id, ingredient.id).length == 0
+        Recipe.create(dessert_id: dessert.id, ingredient_id: ingredient.id)
+      end
+    end
+
+    new_regimes = params[:regimes]
+    dessert.regimes.each do |regime|
+      if (!new_regimes.include?(regime))
+        regime_entry = Regime.find_by(name: regime)
+        Dessert_Regime.where("dessert_id = ? AND regime_id = ?", dessert.id, regime_entry.id).delete_all
+      end
+    end
+    new_regimes.each do |new_regime|
+      regime = Regime.find_or_create_by(name: new_regime)
+      if Dessert_Regime.where("dessert_id = ? AND regime_id = ?", dessert.id, regime.id).length == 0
+        Dessert_Regime.create(dessert_id: dessert.id, regime_id: regime.id)
+      end
+    end
+    dessert.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,98 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+# Regimes
+Regime.create(name: "sugar free")
+Regime.create(name: "paleo")
+Regime.create(name: "keto")
+Regime.create(name: "vegan")
+Regime.create(name: "gluten free")
+
+Ingredient.create(name: "milk")
+Ingredient.create(name: "almond")
+Ingredient.create(name: "egg")
+Ingredient.create(name: "butter")
+Ingredient.create(name: "chocolate")
+Ingredient.create(name: "sugar")
+
 Dessert.create(
-  name: 1,
-  image: 1,
-  carbohydrates: 1,
-  proteins: 1,
-  fat: 1,
-  ingredients: [],
-  regimes: [],
+  "name": "Chocolate Bomb",
+  "image": "image",
+  "carbohydrates": 200,
+  "proteins": 12,
+  "fat": 5,
+)
+Dessert.create(
+  "name": "Strwaberry Keto Bomb",
+  "image": "image",
+  "carbohydrates": 200,
+  "proteins": 12,
+  "fat": 5,
+)
+Dessert.create(
+  "name": "Mango Keto Bomb",
+  "image": "image",
+  "carbohydrates": 200,
+  "proteins": 12,
+  "fat": 5,
+)
+Dessert.create(
+  "name": "chocolate red",
+  "image": "hhtttuuy",
+  "carbohydrates": 25,
+  "proteins": 21,
+  "fat": 20,
+)
+Dessert_Regime.create(
+  dessert_id: 1,
+  regime_id: 1
+)
+Dessert_Regime.create(
+  dessert_id: 1,
+  regime_id: 2
+)
+Dessert_Regime.create(
+  dessert_id: 1,
+  regime_id: 3
+)
+Dessert_Regime.create(
+  dessert_id: 2,
+  regime_id: 1
+)
+Dessert_Regime.create(
+  dessert_id: 2,
+  regime_id: 4
+)
+Dessert_Regime.create(
+  dessert_id: 3,
+  regime_id: 2
+)
+Dessert_Regime.create(
+  dessert_id: 4,
+  regime_id: 2
+)
+Recipe.create(
+  dessert_id: 1,
+  ingredient_id: 1
+)
+Recipe.create(
+  dessert_id: 1,
+  ingredient_id: 2
+)
+Recipe.create(
+  dessert_id: 2,
+  ingredient_id: 3
+)
+Recipe.create(
+  dessert_id: 4,
+  ingredient_id: 1
+)
+Recipe.create(
+  dessert_id: 2,
+  ingredient_id: 4
+)
+Recipe.create(
+  dessert_id: 4,
+  ingredient_id: 3
 )
 puts "✅ Done seeding!!"
